0d8676bf658d19083f78107dd55eb29e945c81d5
fripp9 <164261321+fripp9@users.noreply.github.com>
2024-04-15 22:57:55 -0400

    95% there, missing replay functionality and some fun tweaks.

--- scripts.js
+++ scripts.js
@@ -7,36 +7,69 @@ function getComputerChoice() {
 
 let playerScore = 0;
 let comScore = 0;
-let tieScore = 0;
+let cuantasRondas;
 
 function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
-        return "Empate! esta ronda no cuenta owo"
-    }   else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        return "Perdiste! Papel le gana a Piedra";
-    }   else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-        return "Ganaste! Piedra le gana a Tijeras";
-    }   else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        return "Ganaste! Papel le gana a Piedra";
-    }   else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        return "Perdiste! Tijeras le gana a Papel";
+        resultadoDisplay.textContent = "Empate! esta ronda no cuenta owo";
+        resultadoDisplay.style.color = "gray";
     }   else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        return "Ganaste! Tijeras le gana a Papel";
+        resultadoDisplay.textContent = "Ganaste! Tijeras le gana a Papel";
+        resultadoDisplay.style.color = "green";
+        playerScore++;
+    }   else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        resultadoDisplay.textContent = "Ganaste! Piedra le gana a Tijeras";
+        resultadoDisplay.style.color = "green";
+        playerScore++;
+    }   else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        resultadoDisplay.textContent = "Ganaste! Papel le gana a Piedra";
+        resultadoDisplay.style.color = "green";
+        playerScore++;
+    }   else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        resultadoDisplay.textContent = "Perdiste! Tijeras le gana a Papel";
+        resultadoDisplay.style.color = "red";
+        comScore++;
     }   else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        return "Perdiste! Piedra le gana a Tijeras";
+        resultadoDisplay.textContent = "Perdiste! Piedra le gana a Tijeras";
+        resultadoDisplay.style.color = "red";
+        comScore++;
+    }   else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        resultadoDisplay.textContent = "Perdiste! Papel le gana a Piedra";
+        resultadoDisplay.style.color = "red";
+        comScore++;
     }
-}
+    puntajePlayerDisplay.textContent = playerScore;
+    puntajeComDisplay.textContent = comScore;
+    if (playerScore === cuantasRondas || comScore === cuantasRondas) {
+        imagenes.style.opacity = 0.4;
+        imagen.style.cursor = "default";
+        piedraImg.removeEventListener("click", handlePiedraClick);
+        papelImg.removeEventListener("click", handlePapelClick);
+        tijeraImg.removeEventListener("click", handleTijeraClick);
+        if (playerScore === cuantasRondas) {
+            resultadoDisplay.textContent = "GANASTE! ggwp";
+            resultadoDisplay.style.color = "green";
+            resultadoDisplay.style.fontSize = "70px";
+        }
+        if (comScore === cuantasRondas) {
+            resultadoDisplay.textContent = "Haha perdiste :v";
+            resultadoDisplay.style.color = "red";
+            resultadoDisplay.style.fontSize = "70px";
+    }
 
+}}
 const input = document.querySelector("#inputCell");
 const inputBtn = document.querySelector("#inputBtn");
 const inputLabel = document.querySelector("#inputLabel");
 
 const puntaje = document.querySelector("#puntaje");
-const puntajePlayerDisplay = document.querySelector("puntajePlayerDisplay");
-const puntajeComDisplay = document.querySelector("puntajeComDisplay");
+const puntajePlayerDisplay = document.querySelector("#puntajePlayerDisplay");
+const puntajeComDisplay = document.querySelector("#puntajeComDisplay");
+const resultadoDisplay = document.querySelector("#resultadoDisplay");
 
 const imagenes = document.querySelector("#imagenes");
+const imagen = document.querySelector(".imagen");
 const piedraImg = document.querySelector("#piedra");
 const papelImg = document.querySelector("#papel");
 const tijeraImg = document.querySelector("#tijera");
@@ -48,55 +81,32 @@ function startGame() {
         inputLabel.textContent = "Debes ingresar un numero entre 1 y 100!";
     }   else { 
         if (input.value === "1") {
-            const cuantasRondas = input.value;
+            cuantasRondas = parseInt(input.value);
             inputDiv.style.display = "none";
             inputLabel.textContent = "Gol de oro! " + cuantasRondas + " victoria para ganar!";
             imagenes.style.display = "flex ";
             puntaje.style.display = "flex";   
         }   else {
-            const cuantasRondas = input.value;
+            cuantasRondas = parseInt(input.value);
             inputDiv.style.display = "none";
             inputLabel.textContent = cuantasRondas + " victorias para ganar!";
             imagenes.style.display = "flex ";
             puntaje.style.display = "flex";   
     }} 
 };  
-
-piedraImg.addEventListener("click", () => {
+const handlePiedraClick = () => {
     playRound("ROCK");
-})
+}
+piedraImg.addEventListener("click", handlePiedraClick);
 
-papelImg.addEventListener("click", () => {
+const handlePapelClick = () => {
     playRound("PAPER");
-})
+}
+papelImg.addEventListener("click", handlePapelClick);
 
-tijeraImg.addEventListener("click", () => {
+const handleTijeraClick = () => {
     playRound("SCISSORS");
-})
+}
+tijeraImg.addEventListener("click", handleTijeraClick);
 
 
-// function playGame(cuantasRondas) {
-//     const playerSelection = "ROCK";
-//     const computerSelection = getComputerChoice();
-//     console.log(computerSelection);
-//         let outcome = playRound(playerSelection, computerSelection);
-//         if (outcome.includes("win")) {
-//             playerScore++;
-//         }   else if (outcome.includes("lose")) {
-//             comScore++;
-//         }   else {
-//             tieScore++;
-//         }
-//     }
-
-// function playGame(){
-//     const computerSelection = getComputerChoice();
-//     let outcome = playRound(playerSelection, computerSelection);
-//     if (outcome.includes("win")) {
-//         playerScore++;
-//     }   else if (outcome.includes("lose")) {
-//         comScore++;
-//     }   else {
-//         tieScore++;
-//     }
-// }
